09a68906a442ce7dff4d28b86c944576a3652868
Pseudal <adrien.g@gmx.fr>
2022-11-09 16:27:31 +0100

    chore: add files from IsaacScript 3.3.4 template

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ModCallback } from "isaac-typescript-definitions";
+
+const MOD_NAME = "notccomingdown";
+
+main();
+
+function main() {
+  // Instantiate a new mod object, which grants the ability to add callback functions that
+  // correspond to in-game events.
+  const mod = RegisterMod(MOD_NAME, 1);
+
+  // Register a callback function that corresponds to when a new run is started.
+  mod.AddCallback(ModCallback.POST_GAME_STARTED, postGameStarted);
+
+  // Print a message to the "log.txt" file.
+  Isaac.DebugString(`${MOD_NAME} initialized.`);
+}
+
+function postGameStarted() {
+  Isaac.DebugString("Callback fired: POST_GAME_STARTED");
+}
